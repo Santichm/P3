9a1d267e683a16d62a6372af6e0370bc3a393c06
Santiago Chaparro Martin <santiagochaparromartin@localhost>
2021-11-12 09:57:55 +0100

    Clase Account creada vacía.

---
+++ src/practica3/Account.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica3;
+
+/**
+ *
+ * @author santiagochaparromartin
+ */
+public class Account {
+    
+    int balance;
+    
+    public Account(int balance) {
+        this.balance = balance;
+    }
+    
+    public static int withdraw(int c) {
+        return 0;
+    }
+    
+    public static int deposit(int c) {
+        return 0;
+    }
+}
