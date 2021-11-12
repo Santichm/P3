64e1bb8a6f5b02a33416a2829353ea6959cb111c
Santiago Chaparro Martin <santiagochaparromartin@localhost>
2021-11-12 10:07:08 +0100

    Clase AccountTest creada para que falle.

---
+++ test/practica3/AccountTest.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica3;
+
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author santiagochaparromartin
+ */
+public class AccountTest extends TestCase{
+    
+    public AccountTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of withdraw method, of class Account.
+     */
+    @Test
+    public void testWithdraw() {
+        System.out.println("withdraw");
+        int c = 0;
+        int expResult = 0;
+        int result = Account.withdraw(c);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of deposit method, of class Account.
+     */
+    @Test
+    public void testDeposit() {
+        System.out.println("deposit");
+        int c = 0;
+        int expResult = 0;
+        int result = Account.deposit(c);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+    /**
+     * Test TDD14.
+     */
+    @Test
+    public void testTDD14() {
+        System.out.println("TDD14");
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
